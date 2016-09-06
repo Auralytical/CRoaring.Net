1a1a55dd6b32a824731e66215c59c49a0e459048
RogueException <rogueexception@gmail.com>
2016-09-05 21:57:57 -0300

    Updated windows build script

--- build-win.sh
+++ build-win.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 x86_64-w64-mingw32-gcc -march=native -O3 -std=c11 -shared -o src/CRoaring/CRoaring.dll -fPIC src/CRoaring/roaring.c
+cp src/CRoaring/CRoaring.dll src/CRoaring.Net/
+
+dotnet restore
+dotnet build
