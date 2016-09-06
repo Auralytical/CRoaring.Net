8b596be63f30f7931a3105cb84cbb2b629ae6f14
RogueException <rogueexception@gmail.com>
2016-09-05 21:57:52 -0300

    Updated linux build script

--- build-linux.sh
+++ build-linux.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 x86_64-linux-gnu-gcc -march=native -O3 -std=c11 -shared -o src/CRoaring/libCRoaring.so -fPIC src/CRoaring/roaring.c
-
-cp src/CRoaring/CRoaring.dll src/CRoaring.Net/
 cp src/CRoaring/libCRoaring.so src/CRoaring.Net/
-cp src/CRoaring/CRoaring.dll test/CRoaring.Net.Test/
-cp src/CRoaring/libCRoaring.so test/CRoaring.Net.Test/
 
 dotnet restore
 dotnet build
-dotnet test test/CRoaring.Net.Test
